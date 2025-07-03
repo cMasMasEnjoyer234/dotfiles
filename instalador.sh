dd144ea76c0dba4605b5cad6244f2f609e7ad846
E.C <136939439+cMasMasEnjoyer234@users.noreply.github.com>
2025-07-03 14:44:30 +0000

    Update instalador.sh

--- instalador.sh
+++ instalador.sh
@@ -37,7 +37,7 @@ while true; do
 	esac
 done
 
-sudo pacman -S waybar kitty dunst thunar qt5-wayland qt6-wayland hyprpaper chromium ttf-font-awesome
+sudo pacman -S waybar kitty dunst thunar qt5-wayland qt6-wayland hyprpaper chromium ttf-font-awesome gtk4 gtk3 gtk2
 
 while true; do
 	echo ""
